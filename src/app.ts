ffe92416d9491685dcd85c107ab64cb61f878bcf
gloirekiba <gloirekiba21@gmail.com>
2023-01-28 16:58:28 +0100

    ♻️ refactor code

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,36 @@
-import DICTIONARY from "./dictionary";
+import DICTIONARY from "./dictionary.js";
 
 const numberInput = document.getElementById("numberInput") as HTMLInputElement;
 const result = document.getElementById("result") as HTMLParagraphElement;
 
 numberInput.addEventListener("input", () => {
-  const number = numberInput.value;
-  const words = number.split(" ");
+  const str = numberInput.value.trim();
+
+  if (!str) {
+    result.innerText = "";
+    return;
+  }
+
+  // Replace hyphens with spaces and "and" with spaces
+  // Split the string into an array of words
+  const words = str.replace(/-/g, " ").replace(/ and /g, " ").split(" ");
 
   try {
     const parsedNumber = parseNumber(DICTIONARY, words);
+    result.style.color = "black";
     result.innerText = parsedNumber.toString();
   } catch (err: any) {
+    result.style.color = "red";
     result.innerText = err.message;
   }
 });
 
-console.log(10);
-
-function parseNumber(dict: num[], words: string[]) {
+function parseNumber(dict: num[], words: string[]): number {
   let total = 0;
   let temp = 0;
 
   words.forEach((word) => {
-    const num = dict.find((n) => n.name === word);
+    const num = dict.find((n) => n.name === word.toLowerCase());
     if (!num) throw new Error(`Invalid word: ${word}`);
 
     switch (num.math) {
@@ -30,7 +38,7 @@ function parseNumber(dict: num[], words: string[]) {
         temp += num.value;
         break;
       case "mul":
-        total += temp * num.value;
+        total += (temp || 1) * num.value;
         temp = 0;
         break;
     }
@@ -38,3 +46,15 @@ function parseNumber(dict: num[], words: string[]) {
 
   return total + temp;
 }
+
+/* 
+
+console.log(parseInt("one")); //1
+console.log(parseInt("twenty")); //20
+console.log(parseInt("two hundred forty-six")); //246
+console.log(
+  parseInt("seven hundred eighty-three thousand nine hundred nineteen")
+);
+
+console.log(parseInt("one hundred and twenty three"));
+*/
